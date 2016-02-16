991c7d9b18be9d10f2175ed07d006fa6d7bdbb88
Mehow <Kaiser.mehow@gmail.com>
2016-02-16 22:50:21 +0000

    Making base64 code ready for arbitary int sizes

--- src/hex_to_base64/mod.rs
+++ src/hex_to_base64/mod.rs
@@ -21,15 +21,26 @@ pub fn encode_to_base64(mut raw_binary: Vec<u8>) -> String{
 pub fn encode_24_bits(raw_binary: [u8; 3]) -> String {
     assert!(raw_binary.len() == 3);
     let mut base_64 = "".to_string();
-    let first_6 = raw_binary[0] >> 2;
-    base_64.push(encode_to_char(first_6));
-    let second_6 = ((raw_binary[0] & 0b00000011) << 4) | (raw_binary[1] >> 4);
-    base_64.push(encode_to_char(second_6));
-    let third_6 = ((raw_binary[1] & 0b00001111) << 2) | (raw_binary[2] >> 6);
-    base_64.push(encode_to_char(third_6));
-    let fourth_6 = raw_binary[2] & 0b00111111;
-    base_64.push(encode_to_char(fourth_6));
-    base_64
+    let type_length = 8;
+    let mut bits_left = type_length;
+    let mut index = 0;
+    loop{
+        if bits_left >= 6 {
+            let to_encode = 0b111111 & (raw_binary[index] >> (bits_left - 6));
+            bits_left -= 6;
+            base_64.push(encode_to_char(to_encode));
+        }else{
+            if index == 3 - 1 { return base_64 }
+            let mask = (2 as u8).pow(bits_left + 1) - 1;
+            let top_bits = 0b111111 & ((raw_binary[index] & mask) << (6 - bits_left));
+            bits_left = type_length - (6 - bits_left);
+            let mask = (2 as u8).pow(bits_left + 1) - 1;
+            let bottom_bits = mask & (raw_binary[index + 1] >> bits_left);
+            index += 1;
+            let to_encode = top_bits | bottom_bits;
+            base_64.push(encode_to_char(to_encode));
+        }
+    }
 }
 
 fn encode_to_char(bits: u8) -> char {
@@ -39,6 +50,6 @@ fn encode_to_char(bits: u8) -> char {
         52 ... 61 => char::from_u32('0' as u32 + (bits - 52) as u32).unwrap(),
         62 => '+',
         63 => '/',
-        _ => panic!("{} cannot be encoded to a base 64 character"),
+        _ => panic!("{} cannot be encoded to a base 64 character", bits),
     }
 }
