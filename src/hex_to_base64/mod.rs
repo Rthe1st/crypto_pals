a835641acd54bd14b45a64bb3c8899eb8d7622b6
Mehow <Kaiser.mehow@gmail.com>
2016-02-20 16:45:28 +0000

    improve tests and added generics

--- src/hex_to_base64/mod.rs
+++ src/hex_to_base64/mod.rs
@@ -1,46 +1,67 @@
 #[cfg(test)]
 mod tests;
 
+extern crate num;
+
+use self::num::traits::PrimInt;
+use self::num::ToPrimitive;
+use self::num::FromPrimitive;
+use std::mem::size_of;
 use std::char;
 
-pub fn encode_to_base64(mut raw_binary: Vec<u8>) -> String{
-    loop{
-        match raw_binary.len() % 3{
-            0 => break,
-            _ => raw_binary.push(0 as u8),
-        }
+pub fn hex_decode<T: PrimInt + FromPrimitive + ToPrimitive>(hex:&str) -> Vec<T>{
+    let num_of_4_bits = (size_of::<T>()*8) / 4;
+    let mut hex_bytes:Vec<T> = Vec::new();
+    let zero:T = FromPrimitive::from_u64(0).unwrap();
+    hex_bytes.resize(hex.len()/num_of_4_bits,  zero);
+    for (index, character) in hex.chars().enumerate(){
+        let modulus = index % num_of_4_bits;
+        let shift = 4 * ((num_of_4_bits - 1) - modulus);
+        let decoded_hex:T = FromPrimitive::from_u32(character.to_digit(16).unwrap()).unwrap();
+        let hex_index = index/num_of_4_bits;
+        hex_bytes[hex_index] = hex_bytes[hex_index] | (decoded_hex << shift);
     }
-    let mut base_64 = "".to_string();
-    for index in (0 ..raw_binary.len()).filter(|&x| x % 3 == 0){
-        let encoded_bytes = encode_24_bits([raw_binary[index + 0],raw_binary[index + 1], raw_binary[index + 2]]);
-        base_64.push_str(&encoded_bytes);
-    }
-    base_64
+    hex_bytes
 }
 
-pub fn encode_24_bits(raw_binary: [u8; 3]) -> String {
-    assert!(raw_binary.len() == 3);
-    let mut base_64 = "".to_string();
-    let type_length = 8;
-    let mut bits_left = type_length;
-    let mut index = 0;
-    loop{
-        if bits_left >= 6 {
-            let to_encode = 0b111111 & (raw_binary[index] >> (bits_left - 6));
-            bits_left -= 6;
-            base_64.push(encode_to_char(to_encode));
-        }else{
-            if index == 3 - 1 { return base_64 }
-            let mask = (2 as u8).pow(bits_left + 1) - 1;
-            let top_bits = 0b111111 & ((raw_binary[index] & mask) << (6 - bits_left));
-            bits_left = type_length - (6 - bits_left);
-            let mask = (2 as u8).pow(bits_left + 1) - 1;
-            let bottom_bits = mask & (raw_binary[index + 1] >> bits_left);
-            index += 1;
-            let to_encode = top_bits | bottom_bits;
-            base_64.push(encode_to_char(to_encode));
-        }
+pub fn make_length_multiple_of_3<T: FromPrimitive>(raw_binary: &mut Vec<T>){
+    while raw_binary.len() % 3 != 0{
+        raw_binary.push(FromPrimitive::from_u64(0).unwrap());
     }
+}
+
+fn handle_carried_bits<T: PrimInt + FromPrimitive + ToPrimitive>(carried_over_bits:T, previous_bits_left:usize, binary_element:T) -> (u8, usize){
+    let type_length = size_of::<T>() * 8;
+    let bits_left = type_length - (6 - previous_bits_left);
+    let bottom_bits =  binary_element >> bits_left;
+    let to_encode = carried_over_bits | bottom_bits;
+    (ToPrimitive::to_u8(&to_encode).unwrap(), bits_left)
+}
+
+//we encode 6 bits at a time, thus we need a multiple of 6 to encode
+//as long as T has 2^x bits, and vector has length 3*y:
+// (2^x)*3*y = (2^(x-1))*y*6 will always be a multiple of 6
+//so check size of T is a power of 2, then ensure number fo elements is a multipel of 3
+pub fn encode_to_base64<T: PrimInt + FromPrimitive + ToPrimitive>(raw_binary: Vec<T>) -> String {
+    assert!(size_of::<T>().count_ones() == 1);
+    let mut local_raw_binary = raw_binary.clone();
+    make_length_multiple_of_3(&mut local_raw_binary);
+    let mut base_64 = "".to_string();
+    let mut carried_over_bits: T = FromPrimitive::from_u64(0).unwrap();
+    let mut previous_bits_left = 0;
+    for index in 0..local_raw_binary.len(){
+        let (to_encode, mut bits_left) = handle_carried_bits(carried_over_bits, previous_bits_left, local_raw_binary[index]);
+        base_64.push(encode_to_char(ToPrimitive::to_u8(&to_encode).unwrap()));
+        let six_bit_mask:T = FromPrimitive::from_u64(0b111111).unwrap();
+        while bits_left >= 6 {
+            let to_encode = six_bit_mask & (local_raw_binary[index] >> (bits_left - 6));
+            bits_left -= 6;
+            base_64.push(encode_to_char(ToPrimitive::to_u8(&to_encode).unwrap()));
+        }
+        carried_over_bits = six_bit_mask & (local_raw_binary[index] << (6 - bits_left));
+        previous_bits_left = bits_left;
+    }
+    return base_64;
 }
 
 fn encode_to_char(bits: u8) -> char {
--- src/hex_to_base64/tests.rs
+++ src/hex_to_base64/tests.rs
@@ -1,16 +1,24 @@
 use hex_to_base64;
 
 #[test]
-fn it_works() {
-    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+fn hex_decode_test(){
+    let hex = "49276d206b696c6c";
+    let u8_bin = vec![0b01001001,0b00100111,0b01101101,0b00100000,0b01101011,0b01101001,0b01101100,0b01101100];
+    assert_eq!(format!("{:?}",u8_bin), format!("{:?}",hex_to_base64::hex_decode::<u8>(hex)));
+    let u16_bin = vec![0b0100100100100111,0b0110110100100000,0b0110101101101001,0b0110110001101100];
+    assert_eq!(format!("{:?}",u16_bin), format!("{:?}",hex_to_base64::hex_decode::<u16>(hex)));
+    let u32_bin = vec![0b01001001001001110110110100100000,0b01101011011010010110110001101100];
+    assert_eq!(format!("{:?}",u32_bin), format!("{:?}",hex_to_base64::hex_decode::<u32>(hex)));
+    let u64_bin = vec![0b0100100100100111011011010010000001101011011010010110110001101100 as u64];
+    assert_eq!(format!("{:?}",u64_bin), format!("{:?}",hex_to_base64::hex_decode::<u64>(hex)));
+}
 
-    let mut hex_bytes:Vec<u8> = vec![0;96/2];
-    for (index, character) in hex.chars().enumerate(){
-        let modulus = index % 2;
-        let shift = 4 * (1 - modulus);
-        hex_bytes[index/2] = hex_bytes[index/2] | (character.to_digit(16).unwrap() as u8) << shift;
-    }
-    let base_64ed = hex_to_base64::encode_to_base64(hex_bytes);
-    println!("{}",base_64ed);
-    assert_eq!(base_64ed, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+#[test]
+fn based_64_encode_test() {
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let encoded = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    assert_eq!(encoded,hex_to_base64::encode_to_base64(hex_to_base64::hex_decode::<u8>(hex)));
+    assert_eq!(encoded,hex_to_base64::encode_to_base64(hex_to_base64::hex_decode::<u16>(hex)));
+    assert_eq!(encoded,hex_to_base64::encode_to_base64(hex_to_base64::hex_decode::<u32>(hex)));
+    assert_eq!(encoded,hex_to_base64::encode_to_base64(hex_to_base64::hex_decode::<u64>(hex)));
 }
