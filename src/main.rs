68126be7f6ff99343b98a71debf895de0ebc05fa
Mehow <Kaiser.mehow@gmail.com>
2016-11-19 15:12:03 +0000

    repeating key xor encryption

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod generic_hex_to_base64;
 mod fixed_xor;
 mod single_byte_xor_cracker;
 mod display_helpers;
+mod repeating_xor;
 
 fn main() {
 }
---
+++ src/repeating_xor/mod.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod tests;
+
+pub fn repeating_xor(text: Vec<u8>, key: Vec<u8>) -> Vec<u8>{
+    let mut result = Vec::new();
+    for (text_index, text_byte) in text.iter().enumerate(){
+        let key_byte = key[text_index % key.len()];
+        result.push(text_byte ^ key_byte);
+    }
+    result
+}
