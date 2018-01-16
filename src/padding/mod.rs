ac93ed5790f5cc62d4b33fb07fb5ea4c01346713
Mehow <Kaiser.mehow@gmail.com>
2018-01-16 23:43:31 +0000

    padding improvements

--- src/padding/mod.rs
+++ src/padding/mod.rs
@@ -1,13 +1,19 @@
-use std::error::Error;
-
-pub fn pkcs_7(unpadded: Vec<u8>, block_size: u8) -> Result<Vec<u8>, Box<Error>>{
-    if (unpadded.len() as u8) > block_size {
-        return Err(From::from("pkcs_7 is undefined for block sizes that may needed more then 256 bytes of padding"));
+pub fn pkcs_7(mut unpadded: Vec<u8>, block_size: u8) -> Vec<u8>{
+    //usize % (u8 as usize) will never overflow u8
+    let left_over_bytes = (unpadded.len() % block_size as usize) as u8;
+    let mut padding_needed = block_size - left_over_bytes;
+    if padding_needed == 0 {
+        padding_needed = block_size;
     }
-    let mut with_padding = unpadded.clone();
-    let padding_needed = block_size - (unpadded.len() as u8);
     for _ in 0 .. padding_needed {
-        with_padding.push(padding_needed);
+        unpadded.push(padding_needed);
     }
-    Ok(with_padding)
+    unpadded
+}
+
+pub fn remove_pkcs_7(mut padded: Vec<u8>) -> Vec<u8>{
+    let padding_size = (*padded.last().unwrap()) as usize;
+    let left_over_size = padded.len() - padding_size;
+    padded.truncate(left_over_size);
+    padded
 }
--- tests/set_2_block_crypto.rs
+++ tests/set_2_block_crypto.rs
@@ -6,5 +6,5 @@ use self::crypto_pals::display_helpers::{ u8_vec_to_string };
 #[test]
 fn challenge_9_implement_pkcs_7_padding(){
     let plain_text = String::from("YELLOW SUBMARINE");
-    assert_eq!("YELLOW SUBMARINE\x04\x04\x04\x04", u8_vec_to_string(&pkcs_7(plain_text.into_bytes(), 20).unwrap()));
+    assert_eq!("YELLOW SUBMARINE\x04\x04\x04\x04", u8_vec_to_string(&pkcs_7(plain_text.into_bytes(), 20)));
 }
