dc8954ac447bd1b36559350facf1d134715737b3
Mehow <Kaiser.mehow@gmail.com>
2018-01-17 00:36:42 +0000

    initial cbc implementation

--- src/aes/mod.rs
+++ src/aes/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 extern crate crypto;
 extern crate base64;
 
@@ -47,4 +50,36 @@ pub fn encrypt_ecb(plain_text: &[u8], key: &[u8]) -> Vec<u8>{
         }
     }
     cipher_text
+}
+
+pub fn encrypt_cbc(plain_text: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8>{
+    //todo: through an error if not padded to block multiple
+    let mut previous_block = iv.to_vec();
+    let mut cipher_text = Vec::new();
+    for plain_text_block in plain_text.chunks(128/8){
+        let mut xored_block = Vec::new();
+        for (plain_text_byte, previous_block_byte) in plain_text_block.iter().zip((&previous_block).iter()){
+            xored_block.push(plain_text_byte ^ previous_block_byte);
+        }
+        let cipher_text_block = encrypt_ecb(&xored_block, key);
+        cipher_text.append(& mut cipher_text_block.clone());
+        previous_block = cipher_text_block;
+    }
+    cipher_text
+}
+
+pub fn decrypt_cbc(cipher_text: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8>{
+    //todo: through an error if not padded to block multiple
+    let mut previous_block = iv.to_vec();
+    let mut plain_text = Vec::new();
+    for cipher_text_block in cipher_text.chunks(128/8){
+        let xored_block = decrypt_ecb(&cipher_text_block, key);
+        let mut plain_text_block = Vec::new();
+        for (xored_byte, previous_block_byte) in xored_block.iter().zip((&previous_block).iter()){
+            plain_text_block.push(xored_byte ^ previous_block_byte);
+        }
+        plain_text.append(& mut plain_text_block.clone());
+        previous_block = cipher_text_block.to_vec();
+    }
+    plain_text
 }
--- src/aes/tests.rs
+++ src/aes/tests.rs
@@ -1,9 +1,22 @@
+use aes::{encrypt_ecb, decrypt_ecb, encrypt_cbc, decrypt_cbc};
+
 #[test]
-fn test_encrypt_decrypt(){
+fn test_encrypt_decrypt_ebc(){
     let correct_plain_text = b"QWERTYASDFGZXCVB";
     let key = b"YELLOW SUBMARINE";
-    let cipher_text = aes_in_ecb::encrypt_ecb(&correct_plain_text, key);
-    let plain_text = aes_in_ecb::decrypt_ecb(&cipher_text, key);
+    let cipher_text = encrypt_ecb(correct_plain_text, key);
+    let plain_text = decrypt_ecb(&cipher_text, key);
+    assert_eq!(plain_text, correct_plain_text);
+
+}
+
+#[test]
+fn test_encrypt_decrypt_cbc(){
+    let correct_plain_text = b"QWERTYASDFGZXCVB";
+    let key = b"YELLOW SUBMARINE";
+    let iv = b"qweqweqweqqwewwe";
+    let cipher_text = encrypt_cbc(correct_plain_text, key, iv);
+    let plain_text = decrypt_cbc(&cipher_text, key, iv);
     assert_eq!(plain_text, correct_plain_text);
 
 }
